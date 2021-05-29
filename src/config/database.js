e70fa05b13d70cddf78d9333e69c76d1b71259f0
valtercio santiago <juniorvaltercio2000@gmail.com>
2021-05-28 23:24:05 -0300

    iniciando projeto

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports={
+  "development": {
+    "username": "root",
+    "password": "",
+    "database": "escola",
+    "host": "localhost",
+    "dialect": "mysql",
+    "defile":{
+      "timestamp":true,
+      "underscored":true,
+      "underscoredAll":true
+    }
+  }
+}
